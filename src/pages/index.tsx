08ae825f9db5d3606724b7ef84ce85bd6c9067e6
kjmin <leicht@kjmins-MacBook-Pro.local>
2020-09-18 23:35:08 +0900

    Landing Page

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import '../styles/index.css';
+
+function Index() {
+  return (
+    <main>
+      <Helmet>
+        <title>Kyoungjun Min(kjmin)</title>
+      </Helmet>
+      <h1>Kyoungjun Min(kjmin)</h1>
+      <h2>Contact</h2>
+      <span>kjmin.kr@gmail.com</span>
+      <span>kjmin@kjmin.io</span>
+      <span><a href="https://github.com/schnellehand">github/schnellehand</a></span>
+      <span><a href="https://www.linkedin.com/in/schnellehand/">linkedin/schnellehand</a></span>
+      <span></span>
+    </main>
+  );
+}
+
+export default Index;
