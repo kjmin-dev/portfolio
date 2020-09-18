dc3aa38be6cc85b4b11a98c5ce4e674444b7fda4
kjmin <leicht@kjmins-MacBook-Pro.local>
2020-09-19 00:01:52 +0900

    404

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import '../styles/index.css';
+
+function Index() {
+  return (
+    <main>
+      <Helmet>
+        <title>Kyoungjun Min(kjmin)</title>
+      </Helmet>
+      <h2>404 Page Not Found</h2>
+    </main>
+  );
+}
+
+export default Index;
