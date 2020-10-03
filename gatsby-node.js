f9399e1d85e31f016e30f7c7e6545ce65c00f375
kjmin <leicht@kjmins-MacBook-Pro.local>
2020-10-04 01:31:48 +0900

    react-material && tsconfig

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+const { resolve } = require('path')
+
+exports.onCreateWebpackConfig = function ({ actions }) {
+    actions.setWebpackConfig({
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src')
+            }
+        }
+    })
+}
