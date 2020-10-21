402d46d70c9935475fe84c73c391f890f087eea6
kjmin <leicht@kjmins-MacBook-Pro.local>
2020-10-22 02:30:15 +0900

    global page config _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import config from 'react-reveal/globals'
 
 import 'normalize.css'
 import '@/styles/css/global.css'
-
-config({ ssrFadeout: true })
 
 export default class _App extends React.Component {
     constructor(props) {
